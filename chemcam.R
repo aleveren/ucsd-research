35fe367905f6652dbf4850c7e7463050ef817616
Andrew Leverentz <aleverentz@noreply>
2015-10-25 15:15:30 -0700

    Process CCS and RDR and accumulate results

--- chemcam.R
+++ chemcam.R
@@ -15,32 +15,35 @@ indices <- which(df$EDR.Type == "CL5" & df$PDS. != "No")
 df <- df[indices, , drop=FALSE]
 print(nrow(df))
 
-N <- 3 # TODO nrow(df)
-results <- data.frame(
-  EDR.Filename = rep("", N),
-  Sol = rep(NA, N),
-  RDR.Filename = rep("", N),
-  Download.Time = rep(NA, N),
-  Load.Time = rep(NA, N),
-  File.Num.Rows = rep(NA, N),
-  File.Num.Cols = rep(NA, N),
-  stringsAsFactors = FALSE)
+for (detailType in c("RDR", "CCS")) {
+  N <- 3 # TODO nrow(df)
+  results <- data.frame(
+    EDR.Filename = rep("", N),
+    Sol = rep(NA, N),
+    Detail.Filename = rep("", N),
+    Download.Time = rep(NA, N),
+    Load.Time = rep(NA, N),
+    File.Num.Rows = rep(NA, N),
+    File.Num.Cols = rep(NA, N),
+    stringsAsFactors = FALSE)
 
-for (rowIndex in 1:N) {
-  cat(paste0("Row ", rowIndex, " of ", N, "\n"))
+  accumData <- data.frame()
 
-  row <- df[rowIndex, ]
+  for (rowIndex in 1:N) {
+    cat(paste0("Row ", rowIndex, " of ", N, "\n"))
 
-  rdrFilename <- gsub("M1.DAT", "P3.CSV", gsub("EDR", "RDR", row$EDR.Filename))
-  paddedSol <- sprintf("%05d", row$Sol)
-  toDownload <- paste0(baseUrl, "/data/sol", paddedSol, "/", rdrFilename)
-  localFile <- paste0("data/", rdrFilename)
+    row <- df[rowIndex, ]
 
-  results[rowIndex, "EDR.Filename"] <- row$EDR.Filename
-  results[rowIndex, "Sol"] <- row$Sol
-  results[rowIndex, "RDR.Filename"] <- rdrFilename
+    detailFilename <- gsub("M1.DAT", "P3.CSV",
+        gsub("EDR", detailType, row$EDR.Filename))
+    paddedSol <- sprintf("%05d", row$Sol)
+    toDownload <- paste0(baseUrl, "/data/sol", paddedSol, "/", detailFilename)
+    localFile <- paste0("data/", detailFilename)
 
-  #if (!file.exists(localFile)) {
+    results[rowIndex, "EDR.Filename"] <- row$EDR.Filename
+    results[rowIndex, "Sol"] <- row$Sol
+    results[rowIndex, "Detail.Filename"] <- detailFilename
+
     failed <- FALSE
     tryCatch({
       elapsed <- system.time(download.file(toDownload, localFile))
@@ -50,18 +53,51 @@ for (rowIndex in 1:N) {
     })
     if (!failed) {
       results[rowIndex, "Download.Time"] <- elapsed[[3]]
-    }
-  #}
+      elapsed <- system.time({
+        detailData <- read.csv(localFile, skip = 16)
+      })
 
-  elapsed <- system.time({
-    rdrData <- read.csv(localFile, skip = 16)
-  })
-  print(colnames(rdrData))
-  results[rowIndex, "Load.Time"] <- elapsed[[3]]
-  results[rowIndex, "File.Num.Rows"] <- nrow(rdrData)
-  results[rowIndex, "File.Num.Cols"] <- ncol(rdrData)
+      # Delete detail files to save space
+      file.remove(localFile)
+    }
+
+    results[rowIndex, "Load.Time"] <- elapsed[[3]]
+    results[rowIndex, "File.Num.Rows"] <- nrow(detailData)
+    results[rowIndex, "File.Num.Cols"] <- ncol(detailData)
+
+    wavelengths <- detailData[, 1]
+    detailData[, 1] <- NULL
+    shotNames <- colnames(detailData)
+    detailData <- t(detailData)
+    colnames(detailData) <- paste0("Wave.", wavelengths)
+    rownames(detailData) <- NULL
+
+    shotNumbers <- as.integer(gsub("shot", "", shotNames))
+
+    detailData <- data.frame(
+        Detail.Filename = detailFilename,
+        Sol = row$Sol,
+        Shot = shotNumbers,
+        detailData,
+        stringsAsFactors = FALSE)
+    detailData <- detailData[shotNames != "median" & shotNames != "mean", ]
+
+    print(dim(detailData))
+
+    accumData <- rbind(accumData, detailData)
+  }
+  print(dim(accumData))
+  print(results)
+
+  downloadStatsFile <- paste0("data/downloadStats", detailType, ".csv")
+  write.csv(results, downloadStatsFile, row.names = FALSE)
+
+  accumFile <- paste0("data/accumData", detailType, ".csv.bz2")
+  write.csv(accumData, file = pipe(paste0("bzip2 -c > ", accumFile)),
+      row.names = FALSE)
+  # NOTE: to view this file, use a command like the following:
+  # bzip2 -c -d data/accumDataRDR.csv.bz2 | less -S
 }
-print(results)
 
 mocFiles <- c(
   "moc_0000_0179.csv",
