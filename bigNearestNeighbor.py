cb4a0b50329717dff720f82d8ac09b350b65f5a6
Andrew Leverentz <aleverentz@noreply>
2015-11-11 20:44:39 -0800

    [in progress] attempting batches of projections, adding a few tests

--- bigNearestNeighbor.py
+++ bigNearestNeighbor.py
@@ -3,9 +3,10 @@
 import numpy as np
 import pandas as pd
 import tempfile
+from collections import namedtuple
 
 def selectQuantile(values, alpha):
-  rank = round(len(values) * alpha)
+  rank = int(len(values) * alpha) - 1
   return selectRank(values, rank)
 
 def selectRank(values, rank):
@@ -43,43 +44,82 @@ def randomUnitVector(n):
   unit = v / np.linalg.norm(v)
   return unit
 
-def makeTree(data, maxLeafSize, distanceFunction):
+def makeTree(data, maxLeafSize, distanceFunction, depthPerBatch):
   if not data.numRowsExceeds(maxLeafSize):
     return LazyLeaf(data, distanceFunction)
-  rule = chooseRule(data)
-  leftSelections = data.applyToRows(rule)
-  leftTree = makeTree(data.subset(leftSelections), maxLeafSize,
-      distanceFunction)
-  rightTree = makeTree(data.subset(np.logical_not(leftSelections)), maxLeafSize,
-      distanceFunction)
+
+  numCols = data.numActiveColumns()
+
+  # Compute multiple projections in a single pass through the data
+  numVectors = 2 ** (depthPerBatch + 1) - 1
+  vectors = [randomUnitVector(numCols) for i in range(numVectors)]
+  multiDotProduct = lambda x: [np.dot(v, x) for v in vectors]
+  projections = data.applyToRows(multiDotProduct)
+  indices = np.arange(0, projections.shape[0], [projections.shape[0], 1])
+  projections = np.hstack(indices, projections)
+
+  quantiles = np.random.uniform(0.25, 0.75, numVectors)
+
+  # Compute split points
+  rulesTree = projectionsToRulesTree(projections, vectors, quantiles,
+      maxLeafSize, columnIndex = 1)
+
+  assert isinstance(rulesTree, Node)
+
+  del projections
+  del indices
+
+  # TODO: partition the data
+  # TODO: build tree recursively
+  return None
+
+def projectionsToRulesTree(
+    projections, directions, quantiles, maxLeafSize, columnIndex):
+  # Note: column 0 stores row-index; actual projections start at column 1
+  numRows, numColumns = projections.shape
+  assert numColumns == len(quantiles) + 1
+  assert numColumns == len(directions) + 1
+
+  if columnIndex >= numColumns or numRows <= maxLeafSize:
+    # This will be a leaf node in the intermediate result, either because we
+    # don't have enough precomputed projections, or because the number of rows
+    # is small enough.
+    # Note: The resulting tree contains a vector of indices at each leaf.
+    return projections[:, 0]
+
+  quantile = quantiles[columnIndex - 1]
+  direction = directions[columnIndex - 1]
+  split = selectQuantile(projections[:, columnIndex], quantile)
+  rule = Rule(direction, split)
+
+  leftMask = projections[:, columnIndex] <= split
+  leftProjections = projections[leftMask, :]
+  rightMask = projections[:, columnIndex] > split
+  rightProjections = projections[rightMask, :]
+
+  leftChildIndex = 2*columnIndex
+  rightChildIndex = 2*columnIndex + 1
+
+  leftTree = projectionsToRulesTree(leftProjections, directions, quantiles,
+      maxLeafSize, leftChildIndex)
+  rightTree = projectionsToRulesTree(rightProjections, directions, quantiles,
+      maxLeafSize, rightChildIndex)
+
   return Node(rule, leftTree, rightTree)
 
-def makeForest(data, maxLeafSize, numTrees, distanceFunction):
-  trees = [makeTree(data, maxLeafSize, distanceFunction)
+def makeForest(data, maxLeafSize, numTrees, distanceFunction, depthPerBatch):
+  trees = [makeTree(data, maxLeafSize, distanceFunction, depthPerBatch)
       for i in range(numTrees)]
   return NearestNeighborForest(trees, distanceFunction)
 
-def chooseRule(data):
-  ncols = data.numActiveColumns()
-  u = randomUnitVector(ncols)
-  beta = np.random.uniform(0.25, 0.75)
-  proj = data.applyToRows(lambda x: np.dot(u, x))
-  split = selectQuantile(proj, beta)
-  return Rule(u, split)
-
-class Rule(object):
-  def __init__(self, direction, threshold):
-    self.direction = direction
-    self.threshold = threshold
-
+class Rule(namedtuple("Rule", ["direction", "threshold"])):
   def __call__(self, row):
     '''Apply this rule to a row of data'''
     return np.dot(self.direction, row) <= self.threshold
 
-class LazyLeaf(object):
-  def __init__(self, lazyData, distanceFunction):
-    self.lazyData = lazyData
-    self.distanceFunction = distanceFunction
+class LazyLeaf(namedtuple("LazyLeaf", ["lazyData", "distanceFunction"])):
+  def leafIter(self):
+    yield self
 
   def getLeaf(self, row):
     return self
@@ -90,11 +130,12 @@ class LazyLeaf(object):
     matrix = dataFrame.iloc[:, self.lazyData.columnSlice].values
     return linearScanNearestNeighbor(query, matrix, self.distanceFunction)
 
-class Node(object):
-  def __init__(self, rule, leftTree, rightTree):
-    self.rule = rule
-    self.leftTree = leftTree
-    self.rightTree = rightTree
+class Node(namedtuple("Node", ["rule", "leftTree", "rightTree"])):
+  def leafIter(self):
+    for leaf in self.leftTree.leafIter():
+      yield leaf
+    for leaf in self.rightTree.leafIter():
+      yield leaf
 
   def getLeaf(self, row):
     if self.rule(row):
@@ -105,6 +146,19 @@ class Node(object):
   def nearestNeighbor(self, query):
     leaf = self.getLeaf(query)
     return leaf.nearestNeighbor(query)
+
+  def mapPathsToLeaves(self, pathSoFar = ""):
+    pathToLeft = pathSoFar + "L"
+    pathToRight = pathSoFar + "R"
+    if isinstance(self.leftTree, Node):
+      leftMap = self.leftTree.mapPathsToLeaves(pathToLeft)
+    else:
+      leftMap = {pathToLeft: self.leftTree}
+    if isinstance(self.rightTree, Node):
+      rightMap = self.rightTree.mapPathsToLeaves(pathToRight)
+    else:
+      rightMap = {pathToRight: self.rightTree}
+    return dict(leftMap.items() + rightMap.items())
 
 class NearestNeighborForest(object):
   def __init__(self, trees, distanceFunction):
@@ -210,8 +264,8 @@ if __name__ == "__main__":
   print(result.shape)
 
   print("Building trees")
-  forest = makeForest(exampleData, maxLeafSize = 100, numTrees = 1,
-      distanceFunction = euclidean)
+  forest = makeForest(exampleData, maxLeafSize = 1000, numTrees = 1,
+      distanceFunction = euclidean, depthPerBatch = 2)
   print("Running query")
   result = forest.nearestNeighbor(u)
   print(result)
---
+++ bigTest.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+import unittest
+import numpy as np
+from bigNearestNeighbor import (
+    selectRank, makeForest, euclidean, Node, Rule,
+    projectionsToRulesTree)
+
+class TestNearestNeighbor(unittest.TestCase):
+
+  def test_projectionsToRulesTree_simple(self):
+    projections = np.array([
+        [0, 0],
+        [1, 70],
+        [2, 20],
+        [3, 30],
+        [4, 40],
+        [5, 50],
+        [6, 60],
+        [7, 10],
+    ])
+    directions = [np.ones(10)]
+    quantiles = [0.5]
+    maxLeafSize = 1
+    result = projectionsToRulesTree(
+        projections, directions, quantiles, maxLeafSize, columnIndex = 1)
+    expected = Node(Rule(np.ones(10), 30),
+        np.array([0, 2, 3, 7]),
+        np.array([1, 4, 5, 6]))
+
+    np.testing.assert_equal(expected, result)
+
+  def test_projectionsToRulesTree_withDepth(self):
+    projections = np.array([
+        [0,  0, 24,  0],
+        [1, 70,  0, 31],
+        [2, 20, 23,  0],
+        [3, 30, 22,  0],
+        [4, 40,  0, 34],
+        [5, 50,  0, 32],
+        [6, 60,  0, 33],
+        [7, 10, 21,  0],
+    ])
+    directions = [np.ones(3), np.ones(3)*2, np.ones(3)*3]
+    quantiles = [0.5, 0.25, 0.75]
+    maxLeafSize = 1
+    result = projectionsToRulesTree(
+        projections, directions, quantiles, maxLeafSize, columnIndex = 1)
+    expected = Node(Rule(np.ones(3), 30),
+        Node(Rule(np.ones(3)*2, 21),
+            np.array([7]),        # <= 50 pctile on dim1, <= 25 pctile on dim2
+            np.array([0, 2, 3])), # <= 50 pctile on dim1,  > 25 pctile on dim2
+        Node(Rule(np.ones(3)*3, 33),
+            np.array([1, 5, 6]),  #  > 50 pctile on dim1, <= 75 pctile on dim3
+            np.array([4])))       #  > 50 pctile on dim1,  > 75 pctile on dim3
+
+    np.testing.assert_equal(expected, result)
+
+  def test_mapPathsToLeaves(self):
+    tree = Node(None, 10, 20)
+    expected = {"L": 10, "R": 20}
+    result = tree.mapPathsToLeaves()
+    self.assertEqual(result, expected)
+
+    tree = Node(None, Node(None, 10, 11), 20)
+    expected = {"LL": 10, "LR": 11, "R": 20}
+    result = tree.mapPathsToLeaves()
+    self.assertEqual(result, expected)
+
+    tree = Node(None,
+        Node(None,
+            10,
+            Node(None, 11, 12)),
+        Node(None,
+            Node(None, 20, 21),
+            22))
+    expected = {"LL": 10, "LRL": 11, "LRR": 12,
+        "RLL": 20, "RLR": 21, "RR": 22}
+    result = tree.mapPathsToLeaves()
+    self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+  unittest.main()
