46d2f5cf5814c460ba8944474f9815ad5b3948b6
Andrew Leverentz <aleverentz@noreply>
2015-11-22 18:26:32 -0800

    Add a couple of new example metrics

--- c/example.cpp
+++ c/example.cpp
@@ -2,16 +2,53 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include <exception>
 #include <boost/random.hpp>
 
 using namespace std;
 
+double earthmover(vector<double> a, vector<double> b) {
+  // TODO: double-check this implementation via testing
+  double aTotal = 0.0;
+  double bTotal = 0.0;
+  for (int i = 0; i < a.size(); i++) {
+    aTotal += max(0.0, a[i]);
+    bTotal += max(0.0, b[i]);
+  }
+  if (aTotal <= 0 || bTotal <= 0) {
+    throw runtime_error("Cannot compute earthmover distance");
+  }
+  double aSum = 0.0;
+  double bSum = 0.0;
+  double result = 0.0;
+  for (int i = 0; i < a.size(); i++) {
+    double aScaled = max(0.0, a[i]) / aTotal;
+    double bScaled = max(0.0, b[i]) / bTotal;
+    aSum += aScaled;
+    bSum += bScaled;
+    result += abs(aSum - bSum);
+  }
+  return result;
+}
+
+double normalizedDotProduct(vector<double> a, vector<double> b) {
+  // TODO: double-check this implementation via testing
+  double numer = abs(dot(a, b));  // TODO: how to handle negative components?
+  double denom = sqrt(magnitude(a) * magnitude(b));
+  if (denom <= 0) {
+    throw runtime_error("Cannot compute normalized dot product");
+  }
+  return 1.0 - numer / denom;
+}
+
 int main(int argc, char** argv) {
   string path = "../data/testdata.csv";
   int columnsToIgnore = 1;
+  string analysis = "sim";
+
   if (argc > 1) {
-    string analysis = string(argv[1]);
+    analysis = string(argv[1]);
     if (analysis == "sim") {
       // Do nothing; this is the default.
     } else if (analysis == "full") {
@@ -25,6 +62,21 @@ int main(int argc, char** argv) {
     }
   }
 
+  DistanceMetric metric = &euclidean;
+  string metricName = "euclidean";
+  if (argc > 2) {
+    metricName = string(argv[2]);
+    if (metricName == "euclidean") {
+      // Do nothing; this is the default.
+    } else if (metricName == "normalizedDotProduct") {
+      metric = &normalizedDotProduct;
+    } else if (metricName == "earthmover") {
+      metric = &earthmover;
+    } else {
+      throw runtime_error(string("Unrecognized metricName: ") + metricName);
+    }
+  }
+
   cout << "Loading data from " << path << " ... " << flush;
   Data data = loadData(path, columnsToIgnore);
 
@@ -33,21 +85,29 @@ int main(int argc, char** argv) {
 
   cout << "Data size: COLS = " << COLS << ", ROWS = " << ROWS << endl;
 
+  vector<double> query(COLS, 0.0);
+  if (analysis == "subset" || analysis == "full") {
+    boost::random::mt19937 gen(1);
+    for (int i = 0; i < COLS; i++) {
+      query[i] = boost::random::uniform_real_distribution<>(0.0, 10.0)(gen);
+    }
+  }
+  cout << "Query vector: " << join(", ", query) << endl;
+
   int maxLeafSize = 500;
   int numTrees = 10;
 
   cout << "Building trees" << endl;
-  Forest *forest = makeForest(data, maxLeafSize, numTrees, &euclidean);
-  vector<double> query(COLS, 0.0);
+  Forest *forest = makeForest(data, maxLeafSize, numTrees, metric);
 
   cout << "Running random-projection query ... " << flush;
   vector<double> *result = forest->nearestNeighbor(query);
-  double resultDistance = euclidean(*result, query);
+  double resultDistance = metric(*result, query);
   cout << "Found point at distance " << resultDistance << endl;
 
   cout << "Running linear scan ... " << flush;
-  result = linearScanNearestNeighbor(data, query, &euclidean);
-  resultDistance = euclidean(*result, query);
+  result = linearScanNearestNeighbor(data, query, metric);
+  resultDistance = metric(*result, query);
   cout << "Found point at distance " << resultDistance << endl;
 
   return 0;
