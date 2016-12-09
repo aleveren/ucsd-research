5f645dcfaaddf8b303254d6ba6f5a3516f2cbf45
Andrew Leverentz <aleverentz@noreply>
2016-12-09 11:32:44 -0800

    Add ability to insert NaN into wide spectrum gaps (for better plotting)

--- spectrum-labeling/data_util.py
+++ spectrum-labeling/data_util.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import re
 
-def row_to_spectrum(row, columns = None):
+def row_to_spectrum(row, columns = None, gap_insert_nan = None):
     if columns is None:
         assert isinstance(row, pd.Series)
         columns = row.index
@@ -12,6 +12,12 @@ def row_to_spectrum(row, columns = None):
     for i, colname in enumerate(columns):
         regex_result = regex.search(colname)
         if regex_result:
-            wavelengths.append(float(regex_result.group(1)))
-            intensities.append(row[i])
+            w = float(regex_result.group(1))
+            y = row[i]
+            if gap_insert_nan is not None and len(wavelengths) > 0 \
+                    and (w - wavelengths[-1]) > gap_insert_nan:
+                wavelengths.append(np.nan)
+                intensities.append(np.nan)
+            wavelengths.append(w)
+            intensities.append(y)
     return np.array(wavelengths), np.array(intensities)
