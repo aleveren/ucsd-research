Fix distance calculation to enforce symmetry, and add tests

hier-clust/hier_clust/hier_clust.py
@@ -5,7 +5,7 @@ import pandas as pd
 from scipy.sparse import coo_matrix, dia_matrix, csr_matrix, issparse
 from sklearn.neighbors import NearestNeighbors, DistanceMetric
 import sklearn.metrics
-from collections import Counter, deque
+from collections import defaultdict, Counter, deque
 import sys
 import os
 import re
@@ -34,7 +34,7 @@ class HierClust(object):
             sparse_similarity = 'auto',
             alpha = 0.8,
             leaf_size = 1,
-            neighbor_graph_strategy = 'rptree',
+            neighbor_graph_strategy = 'balltree',
             metric = 'euclidean',
             full_eigen_threshold = 10,
             convergence_iterations = 10):
@@ -377,9 +377,14 @@ class HierClust(object):
                 distanceFunction = func
             else:
                 distanceFunction = self.metric
-            forest = makeForest(data, maxLeafSize = self.n_neighbors * 2, numTrees = 10, distanceFunction = distanceFunction)
+            forest = makeForest(
+                data,
+                maxLeafSize = self.n_neighbors * 2,
+                numTrees = 10,  # TODO: parameterize
+                distanceFunction = distanceFunction)
             distances, indices = forest.kneighbors(data, k = self.n_neighbors)
-        else:
+
+        elif self.neighbor_graph_strategy == 'balltree':
             nn_obj = NearestNeighbors(
                 n_neighbors = self.n_neighbors,
                 algorithm = 'ball_tree',
@@ -387,10 +392,10 @@ class HierClust(object):
             ).fit(data)
             distances, indices = nn_obj.kneighbors(data)
 
-        row_new = []
-        col_new = []
-        val_new = []
-        entries = Counter()
+        else:  # pragma: no cover
+            raise Exception("Unrecognized neighbor_graph_strategy: {}".format(self.neighbor_graph_strategy))
+
+        entries = defaultdict(list)
         for row_index in xrange(len(data)):
             for nbr_index in xrange(self.n_neighbors):
                col_index = indices[row_index, nbr_index]
@@ -401,21 +406,28 @@ class HierClust(object):
                    dist = np.nan
 
                a, b = min(row_index, col_index), max(row_index, col_index)
-               entries[a, b] += 1
+               entries[a, b].append(dist)
 
-               if a == b or not self.mutual_neighbors:
-                   row_new.append(row_index)
-                   col_new.append(col_index)
-                   val_new.append(dist)
+        row_new = []
+        col_new = []
+        val_new = []
+        for (a, b), dists in entries.items():
+            count = len(dists)
+            assert count == 1 or count == 2
 
-               elif entries[a, b] == 2:
-                   row_new.append(row_index)
-                   col_new.append(col_index)
-                   val_new.append(dist)
+            if a == b:
+                assert count == 1
+                row_new.append(a)
+                col_new.append(b)
+                val_new.append(dists[0])
 
-                   row_new.append(col_index)
-                   col_new.append(row_index)
-                   val_new.append(dist)
+            if a != b and (not self.mutual_neighbors or count == 2):
+                row_new.append(a)
+                col_new.append(b)
+                val_new.append(dists[0])
+                row_new.append(b)
+                col_new.append(a)
+                val_new.append(dists[0])
 
         result = coo_matrix((val_new, (row_new, col_new)), shape=(len(data), len(data)))
         return result

hier-clust/tests/test_hier_clust.py
@@ -35,6 +35,30 @@ class Tests(unittest.TestCase):
         dist2 = hc._get_distances(d)
         assert not issparse(dist2)
 
+        # Check distance calculations
+        z = np.nan
+        expected = np.array([
+            [ z,  1,  0,  0,  0,  0],
+            [ 1,  z,  0,  0,  0,  0],
+            [ 0,  0,  z,  1,  0,  0],
+            [ 0,  0,  1,  z,  0,  0],
+            [ 0,  0,  0,  0,  z,  z],
+            [ 0,  0,  0,  0,  z,  z],
+        ], dtype='float')
+        dist1_squared = np.asarray(np.square(dist1.todense()))
+        assert np.allclose(dist1_squared, expected, equal_nan=True)
+
+        expected = np.array([
+            [ 0,  1,  4,  5, 50, 50],
+            [ 1,  0,  5,  4, 41, 41],
+            [ 4,  5,  0,  1, 34, 34],
+            [ 5,  4,  1,  0, 25, 25],
+            [50, 41, 34, 25,  0,  0],
+            [50, 41, 34, 25,  0,  0],
+        ], dtype='float')
+        assert np.allclose(np.square(dist2), expected)
+
+        # Check similarity calculations
         sim1 = hc._get_similarity(dist1)
         assert issparse(sim1)
         sim2 = hc._get_similarity(dist2)
@@ -53,12 +77,26 @@ class Tests(unittest.TestCase):
         assert not np.allclose(sim1.todense(), sim2)
         assert not np.allclose(sim2, expected)
 
+        expected = np.exp(-dist2 ** 2 / 2.0)
+        assert np.allclose(sim2, expected)
+
     def test_similarity_sparse_mutual(self):
         # Test case where mutual KNN makes a difference
         hc = hier_clust.HierClust(n_neighbors = 2, sigma_similarity = 1.0,
             mutual_neighbors = True)
         d = np.array([[0,0], [1,0], [0,2]])
+
         dist1 = hc._get_distances(d)
+
+        z = np.nan
+        expected = np.array([
+            [z, 1, 0],
+            [1, z, 0],
+            [0, 0, z]])
+        dist1_squared = np.asarray(np.square(dist1.todense()))
+        assert np.allclose(dist1_squared, expected, equal_nan=True)
+        assert dist1.nnz == 5
+
         sim1 = hc._get_similarity(dist1)
 
         x = np.exp(-0.5)
@@ -72,11 +110,22 @@ class Tests(unittest.TestCase):
         # Recompute similarities, allowing non-mutual neighbors
         hc = hier_clust.HierClust(n_neighbors = 2, sigma_similarity = 1.0,
             mutual_neighbors = False)
+
         dist2 = hc._get_distances(d)
+
+        z = np.nan
+        expected = np.array([
+            [z, 1, 4],
+            [1, z, 0],
+            [4, 0, z]])
+        dist2_squared = np.asarray(np.square(dist2.todense()))
+        assert np.allclose(dist2_squared, expected, equal_nan=True)
+        assert dist2.nnz == 7
+
         sim2 = hc._get_similarity(dist2)
 
         x = np.exp(-0.5)
-        y = 0.5 * np.exp(-2)
+        y = np.exp(-2.0)
         expected = np.array([
             [1, x, y],
             [x, 1, 0],