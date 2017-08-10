5a2c3e46106fbb6c5afad080063f4385810f1b82
Andrew Leverentz <aleverentz@noreply>
2017-08-10 00:29:50 -0700

    Reimplementing vi_ncrp code; hopefully faster and more correct

--- vi_ncrp/vi_ncrp.py
+++ vi_ncrp/vi_ncrp.py
@@ -17,6 +17,8 @@ except ImportError:
 class ErrIfNotFound(object):
     pass
 
+_default_branch_factor = 5
+
 class TreeNode(object):
     def __init__(self, children):
         self.children = children
@@ -38,19 +40,35 @@ class TreeNode(object):
             raise ValueError("Path not found")
         return None
 
-    def inner_and_full_paths(self, prefix_so_far = ()):
-        yield prefix_so_far
-        for k, c in enumerate(self.children):
-            new_prefix = tuple(prefix_so_far) + (k,)
-            for p in c.inner_and_full_paths(prefix_so_far = new_prefix):
+    def inner_and_full_paths(self, start_at = None, prefix_so_far = ()):
+        if start_at is not None:
+            prefix_so_far = start_at
+            iterable = self.lookup_path(start_at).inner_and_full_paths(
+                start_at = None, prefix_so_far = prefix_so_far)
+            for p in iterable:
                 yield p
+        else:
+            yield prefix_so_far
+            for k, c in enumerate(self.children):
+                new_prefix = tuple(prefix_so_far) + (k,)
+                for p in c.inner_and_full_paths(start_at = None, prefix_so_far = new_prefix):
+                    yield p
+
+    @classmethod
+    def from_branch_structure(cls, branch_structure):
+        if len(branch_structure) == 0:
+            return cls(children = [])
+        children = [cls.from_branch_structure(branch_structure[1:])
+            for i in range(branch_structure[0])]
+        return cls(children = children)
 
 class NCRP(object):
     def __init__(self, **kwargs):
         self._dict = dict(
             alphaW = 0.1,
             alphaV = 0.1,
-            alphaTheta = np.ones((3,)),
+            alphaTheta = None,
+            branch_structure = None,
             depth = 2,
             iterations = 100,
             progress_bar = None,
@@ -58,12 +76,21 @@ class NCRP(object):
         for k, v in kwargs.items():
             assert k in self._dict, "Unrecognized parameter: {}".format(k)
             self._dict[k] = v
+        # Set a few default values that depend on depth
+        if self._dict["alphaTheta"] is None:
+            self._dict["alphaTheta"] = np.ones((self._dict["depth"] + 1))
+        if self._dict["branch_structure"] is None:
+            self._dict["branch_structure"] = \
+                [_default_branch_factor for i in range(self._dict["depth"])]
+
         assert len(self._dict["alphaTheta"]) == self._dict["depth"] + 1, \
             "Length of alphaTheta must be depth + 1"
+        assert len(self._dict["branch_structure"]) == self._dict["depth"], \
+            "Length of branch_structure must be equal to depth"
 
     def __repr__(self):
         return (self.__class__.__name__ + "("
-            + ", ".join("{}={}".format(k, v) for k, v in self._dict.items())
+            + ", ".join("{}={}".format(k, repr(v)) for k, v in self._dict.items())
             + ")")
 
     def fit(self, data):
@@ -80,11 +107,37 @@ class NCRPFit(object):
         self.n_obs = self.data.shape[0]
         self.vocab_size = self.data.shape[1]
 
-        self.tree = TreeNode(children = [])
-        for i in range(self.depth):
-            self.tree = TreeNode(children = [self.tree])
+    def init_stats(self):
+        self.tree = TreeNode.from_branch_structure(self.branch_structure)
 
-        self.phi_var = (1.0 / self.depth) * np.ones((self.n_obs, self.depth + 1))
+        self.count_paths = 0
+        self.path_to_index = dict()
+        self.index_to_path = dict()
+        for i, p in enumerate(self.tree.inner_and_full_paths()):
+            self.count_paths += 1
+            self.path_to_index[p] = i
+            self.index_to_path[i] = p
+
+        self.word_count_by_doc = np.squeeze(np.asarray(self.data.sum(axis = 1)))
+
+        self.alphaW_var = self.alphaW * np.ones((self.count_paths, self.vocab_size,))
+        self.EqlnW = np.ones((self.count_paths, self.vocab_size))
+
+        self.j0 = np.ones((self.count_paths,), dtype='int')
+        self.l0 = np.ones((self.count_paths,), dtype='int')
+
+        self.alphaV_var = np.ones((self.count_paths,))
+        self.betaV_var = self.alphaV * np.ones((self.count_paths,))
+        self.EqlnV = np.ones((self.count_paths,))
+        self.Eqln1_V = np.ones((self.count_paths,))
+
+        self.z0 = np.ones((self.n_obs, self.count_paths))
+        self.logS = np.ones((self.n_obs, self.count_paths)) + np.random.uniform(0, 0.01, self.z0.shape)
+        self.path_prob = softmax(self.logS, axis=1)
+
+        log_phi_var = np.ones((self.n_obs, self.depth + 1))
+        log_phi_var += np.random.uniform(0, 0.01, log_phi_var.shape)
+        self.phi_var = softmax(log_phi_var, axis=1)
         self.alphaTheta_var = np.ones((self.n_obs, self.depth + 1))
         # TODO: should either or both of these be 3-dimensional arrays?
         # - For example: phi_var[doc, vocab_word, depth]?
@@ -93,180 +146,136 @@ class NCRPFit(object):
         # Define some expected values according to prior distrib p
         self.EplnV = digamma(1) - digamma(1 + self.alphaV)
         self.Epln1_V = digamma(self.alphaV) - digamma(1 + self.alphaV)
-        self.EplnW = np.ones((self.vocab_size,)) * \
-            (digamma(self.alphaW) - digamma(self.vocab_size * self.alphaW))
-
-        self.tree_var_params = dict()
-        self.tree_stats = dict()
+        self.EplnW = digamma(self.alphaW) - digamma(self.vocab_size * self.alphaW)
 
     def fit(self):
-        pbar = self.get_progress_bar(total = self.iterations)
-        self.init_tree_var_params()
-        for iter_index in range(self.iterations):
-            self.update_tree_stats()
-            self.update_doc_stats()
-            # TODO: update tree structure (grow / prune / merge)
-            if pbar is not None:
-                pbar.update(1)
-        if pbar is not None:
-            pbar.close()
+        self.init_stats()
+        fit_desc = "Optimizing via coordinate ascent"
+        for iter_index in self.get_progress_bar(range(self.iterations), desc=fit_desc):
+            self.update()
         return self
 
-    def init_tree_var_params(self):
-        '''Initialize variational params for each node in the tree'''
-        for path in self.tree.inner_and_full_paths():
-            self.tree_var_params[path] = dict(
-                alphaV_var = 1.0,
-                betaV_var = 1.0,
-                alphaW_var = np.ones((self.vocab_size,)),
-            )
+    def update(self):
+        for i, path in enumerate(self.tree.inner_and_full_paths()):
+            self.j0[i] = len(self.tree.lookup_path(path).children)
+            self.l0[i] = len(path)
 
-    def update_doc_stats(self):
-        # Update level Dirichlet distributions (variational params for theta_d)
-        self.alphaTheta_var = self.alphaTheta[np.newaxis, :] + self.phi_var
+        # Update path-specific variational expectations
+        self.EqlnV = digamma(self.alphaV_var) - digamma(self.alphaV_var + self.betaV_var)
+        self.Eqln1_V = digamma(self.betaV_var) - digamma(self.alphaV_var + self.betaV_var)
+        self.EqlnW = digamma(self.alphaW_var) - digamma(np.sum(self.alphaW_var, axis=1, keepdims=True))
+        # The root node is a special case for the stick-breaking proportions
+        root_path_index = self.path_to_index[()]
+        self.EqlnV[root_path_index] = 0.0
+        self.Eqln1_V[root_path_index] = 0.0  # TODO: Technically -infinity?  Should never be used?  OK b/c constant?
 
-        # Update level categorical distributions (variational params for z_d)
-        log_phi_var = np.zeros(self.phi_var.shape)
-        for d in range(self.n_obs):
+        # Compute z0
+        self.z0 = np.zeros(self.z0.shape)
+        for path_index, path in self.index_to_path.items():
+            for k in range(self.depth+1):
+                if k < len(path):
+                    subpath_index = self.path_to_index[path[:k]]
+                    self.z0[:, path_index] += self.phi_var[:, k] * self.data.dot(self.EqlnW[subpath_index, :])
+                else:
+                    self.z0[:, path_index] += self.phi_var[:, k] * self.word_count_by_doc * self.EplnW
+
+        # Compute logS and path probabilities
+        self.logS = (self.z0
+            + (self.depth - self.l0[np.newaxis, :]) * self.EplnV
+            - self.j0[np.newaxis, :] * self.Epln1_V
+            - (self.depth - self.l0[np.newaxis, :]) * np.log(1 - np.exp(self.Epln1_V)))
+        for path_index, path in self.index_to_path.items():
+            for k in range(len(path) + 1):
+                subpath_index = self.path_to_index[path[:k]]
+                self.logS[:, path_index] += self.EqlnV[subpath_index]
+                if 0 < k and k < len(path):
+                    # TODO: double-check the indexing here
+                    for j in range(path[k] - 1):
+                        subpath = tuple(path[:k-1]) + (j,)
+                        subpath_index = self.path_to_index[subpath]
+                        self.logS[:, path_index] += self.Eqln1_V[subpath_index]
+            for j in range(self.j0[path_index]):
+                extended_path_index = self.path_to_index[tuple(path) + (j,)]
+                self.logS[:, path_index] += self.Eqln1_V[extended_path_index]
+        self.path_prob = softmax(self.logS, axis=1)
+
+        # Update variational parameters for distribution over depths
+        self.alphaTheta_var = self.alphaTheta[np.newaxis, :] + self.phi_var * self.word_count_by_doc[:, np.newaxis]
+
+        # Update variational parameters for depth indicators
+        # TODO: experiment with phi_var[d, n, k] instead of phi_var[d, k]
+        EqlnTheta = digamma(self.alphaTheta_var) - digamma(self.alphaTheta_var.sum(axis=1, keepdims=True))
+        log_phi_var = EqlnTheta.copy()
+        for path_index, path in self.index_to_path.items():
             for k in range(self.depth + 1):
-                current = digamma(self.alphaTheta_var[d, k]) - digamma(np.sum(self.alphaTheta_var[d, :]))
-                for path in self.tree.inner_and_full_paths():
-                    for j in range(self.vocab_size):
-                        count = self.data[d, j]
-                        path_prob = self.tree_stats[path]["path_prob"][d]
-                        subpath = path[:k]
-                        eqw = self.tree_stats[subpath]["EqlnW"][j]
-                        current += count * path_prob * eqw
-                log_phi_var[d, k] = current
-        self.phi_var = softmax(log_phi_var)
+                if k < len(path):
+                    subpath_index = self.path_to_index[path[:k]]
+                    term = self.path_prob[:, path_index] * self.data.dot(self.EqlnW[subpath_index, :])
+                else:
+                    term = self.EplnW * self.path_prob[:, path_index] * self.word_count_by_doc
+                log_phi_var[:, k] += term / self.word_count_by_doc
+        self.phi_var = softmax(log_phi_var, axis=1)
 
-    def update_tree_stats(self):
-        count_paths = 0
-        for path in self.tree.inner_and_full_paths():
-            count_paths += 1
-            self.tree_stats[path] = dict()
-            self.tree_stats[path]["j0"] = len(self.tree.lookup_path(path).children)
-            self.tree_stats[path]["EqlnV"] = self.EqlnV(path)
-            self.tree_stats[path]["Eqln1_V"] = self.Eqln1_V(path)
-            self.tree_stats[path]["EqlnW"] = self.EqlnW(path)
-
-        for path in self.tree.inner_and_full_paths():
-            self.tree_stats[path]["z0"] = np.asarray([self.z0(d, path) for d in range(self.n_obs)])
-
-        logS_matrix = np.zeros((count_paths, self.n_obs))
-        for path_index, path in enumerate(self.tree.inner_and_full_paths()):
-            ls = np.asarray([self.logS(d, path) for d in range(self.n_obs)])
-            logS_matrix[path_index, :] = ls
-            self.tree_stats[path]["logS"] = ls
-
-        # Update probabilities of inner & full paths
-        # (variational params for c_d)
-        path_prob_matrix = softmax(logS_matrix, axis=0)
-        for path_index, path in enumerate(self.tree.inner_and_full_paths()):
-            self.tree_stats[path]["path_prob"] = path_prob_matrix[path_index, :]
-
-        # Update topics (variational params for W_path)
-        for path in self.tree.inner_and_full_paths():
-            for j in range(self.vocab_size):
-                current = self.tree_var_params[path]["alphaW_var"][j]
-                for d in range(self.n_obs):
-                    count = self.data[d, j]
-                    term = 0
-                    subtree = self.tree.lookup_path(path)
-                    for path_prime in subtree.inner_and_full_paths(path):
-                        path_prob = self.tree_stats[path_prime]["path_prob"][d]
-                        term += self.phi_var[d, len(path)] * path_prob
-                    current += count * term
-                self.tree_var_params[path]["alphaW_var"][j] = current
-
-        # Update stick-breaking params (variational params for V_path)
-        # TODO: double check this logic
-        for path in self.tree.inner_and_full_paths():
+        # Update variational parameters for stick-breaking proportions
+        self.alphaV_var[:] = 1.0
+        self.betaV_var[:] = self.alphaV
+        for path_index, path in self.index_to_path.items():
+            for child_path in self.tree.inner_and_full_paths(start_at = path):
+                child_path_index = self.path_to_index[child_path]
+                self.alphaV_var[path_index] += np.sum(self.path_prob[:, child_path_index])
             if len(path) == 0:
-                continue
-            alpha = 2.0
-            beta = 1.0 + self.alphaV
-            for d in range(self.n_obs):
-                short_path = path[:-1]
-                subtree = self.tree.lookup_path(path)
-                for path_prime in subtree.inner_and_full_paths(short_path):
-                    path_prob = self.tree_stats[path]["path_prob"][d]
-                    alpha += path_prob
-                    if (len(path_prime) < len(path) or
-                            path_prime[len(path) - 1] > path[-1]):
-                        beta += path_prob
-            self.tree_var_params[path]["alphaV_var"] = alpha
-            self.tree_var_params[path]["betaV_var"] = beta
+                self.betaV_var[path_index] += np.sum(self.path_prob[:, path_index])
+            else:
+                super_path = path[:-1]
+                super_path_index = self.path_to_index[super_path]
+                self.betaV_var[path_index] += np.sum(self.path_prob[:, super_path_index])
+                for r_sibling in range(len(self.tree.lookup_path(super_path).children)):
+                    if r_sibling <= path[-1]:
+                        continue
+                    r_sibling_path = tuple(super_path) + (r_sibling,)
+                    for aux_path in self.tree.inner_and_full_paths(start_at = r_sibling_path):
+                        aux_path_index = self.path_to_index[aux_path]
+                        self.betaV_var[path_index] += np.sum(self.path_prob[:, aux_path_index])
 
-    def get_progress_bar(self, total):
+        # Update variational parameters for topic distributions
+        self.alphaW_var[:, :] = self.alphaW
+        for path_index, path in self.index_to_path.items():
+            current_prob = np.zeros((self.n_obs,))
+            for child_path in self.tree.inner_and_full_paths(start_at = path):
+                child_path_index = self.path_to_index[child_path]
+                current_prob += self.path_prob[:, child_path_index]
+            # TODO: perform this multiplication more efficiently, making use of sparse format
+            prod = (np.asarray(self.data.todense())
+                * self.phi_var[:, len(path), np.newaxis]
+                * current_prob[:, np.newaxis])
+            self.alphaW_var[path_index, :] += np.sum(prod, axis=0)
+
+        # TODO: update tree structure (grow / prune / merge)
+
+    def get_most_likely_paths(self):
+        path_indices_by_doc = np.argmax(self.path_prob, axis=1)
+        most_likely_paths = np.empty((self.n_obs,), dtype='object')
+        for doc_index, p in enumerate(path_indices_by_doc):
+            most_likely_paths[doc_index] = self.index_to_path[p]
+        return most_likely_paths
+
+    def get_progress_bar(self, iterable=None, **kwargs):
+        if iterable is not None:
+            kwargs["iterable"] = iterable
+
         if self.progress_bar == 'notebook' and tqdm is not None:
-            return tqdm.tqdm_notebook(total = total)
+            return tqdm.tqdm_notebook(**kwargs)
         elif self.progress_bar == 'terminal' and tqdm is not None:
-            return tqdm.tqdm(total = total)
+            return tqdm.tqdm(**kwargs)
         else:
-            return None
+            return iterable
 
-    def EqlnV(self, path):
-        # Expectation of V[path], where V[path] ~ variational distrib q
-        alpha = self.tree_var_params[path]["alphaV_var"]
-        beta = self.tree_var_params[path]["betaV_var"]
-        return digamma(alpha) - digamma(alpha + beta)
-
-    def Eqln1_V(self, path):
-        # Expectation of (1-V[path]), where V[path] ~ variational distrib q
-        alpha = self.tree_var_params[path]["alphaV_var"]
-        beta = self.tree_var_params[path]["betaV_var"]
-        return digamma(beta) - digamma(alpha + beta)
-
-    def EqlnW(self, path):
-        # Expectation of W[path, :], where W[path, :] ~ variational distrib q
-        alpha = self.tree_var_params[path]["alphaW_var"]
-        return digamma(alpha) - digamma(np.sum(alpha))
-
-    def z0(self, d, path):
-        '''Compute E_q[log p(t_d | z_d, W, c_d = path)]'''
-        L = self.depth
-        l0 = len(path)
-        if l0 < L:
-            path = tuple(path) + tuple([0 for i in range(L - l0)])
-        result = 0.0
-        for j in range(self.vocab_size):
-            count = self.data[d, j]
-            term = 0.0
-            for k in range(L+1):
-                subpath = path[:k]
-                term += self.phi_var[d, k] * self.tree_stats[subpath]["EqlnW"][j]
-            result += count * term
-        return result
-
-    def logS(self, d, path):
-        result = self.tree_stats[path]["z0"][d]
-        L = self.depth
-        l0 = len(path)
-        for l in range(l0):
-            result += self.tree_stats[path[:l]]["EqlnV"]
-            for j in range(path[l]):
-                new_path = tuple(path[:l-1]) + (j,)
-                result += self.tree_stats[new_path]["Eqln1_V"]
-        if l0 < L:
-            # For inner paths, account for all full paths that leave the
-            # truncated tree immediately after passing through `path`.
-            j0 = self.tree_stats[path]["j0"]
-            result += (L - l0) * self.EplnV
-            result -= j0 * self.Epln1_V
-            result -= (L - l0) * np.log(1 - np.exp(self.Epln1_V))
-            for j in range(j0):
-                new_path = tuple(path) + (j,)
-                result += self.tree_stats[new_path]["Eqln1_V"]
-        return result
-
-def softmax(X, axis=0):
+def softmax(X, axis):
     '''Compute softmax using exp-normalize trick'''
-    X = X.copy()
-    X -= np.max(X, axis=axis, keepdims=True)
-    np.exp(X, out=X)
-    X /= np.sum(X, axis=axis, keepdims=True)
-    return X
+    result = X - np.max(X, axis=axis, keepdims=True)
+    np.exp(result, out=result)
+    result /= np.sum(result, axis=axis, keepdims=True)
+    return result
 
 def main():
     ncrp = NCRP(alpha = 0.1)
