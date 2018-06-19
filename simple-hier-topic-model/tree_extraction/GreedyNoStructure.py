90b5a2b958aeb9328ff1b8eaefc43eeaeddfb23e
Andrew Leverentz <aleverentz@noreply>
2018-06-19 13:04:27 -0600

    Put a couple of tree extraction algorithms into their own subdirectory

---
+++ simple-hier-topic-model/tree_extraction/GreedyNoStructure.py
@@ -0,0 +1,84 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def extract(m, diagnostics = None, depth_penalty = None):
+    remaining_indices = set(range(m.shape[0]))
+    used_indices = set()
+    #cond = m.copy()
+    cond = m / m.sum(axis=0, keepdims=True)
+    root = np.argmax(np.mean(cond, axis=1))
+    depths = {root: 0}
+    remaining_indices.remove(root)
+    used_indices.add(root)
+    if diagnostics is not None:
+        diagnostics.append({"matrix": cond, "raw_matrix": m.copy(), "root": root})
+    g = nx.Graph()
+    g.add_node(root)
+    while len(remaining_indices) > 0:
+        rem_ind_arr = np.array(sorted(list(remaining_indices)))
+        used_ind_arr = np.array(sorted(list(used_indices)))
+        depths_arr = np.array([depths[i] for i in used_ind_arr])
+        cond_submatrix = cond[rem_ind_arr.reshape(-1,1), used_ind_arr.reshape(1,-1)]
+        if depth_penalty is not None:
+            cond_submatrix = cond_submatrix.copy() * (depth_penalty ** depths_arr[np.newaxis, :])
+        argmax = np.argmax(cond_submatrix)
+        ii, jj = np.unravel_index(argmax, cond_submatrix.shape)
+        i = rem_ind_arr[ii]
+        j = used_ind_arr[jj]
+        depths[i] = depths[j] + 1
+        g.add_edge(i, j)
+        if diagnostics is not None:
+            diagnostics.append({"matrix": cond_submatrix, "selection": (ii, jj), "edge": (i, j),
+                "rem_ind_arr": rem_ind_arr, "used_ind_arr": used_ind_arr, "depths_arr": depths_arr})
+        remaining_indices.remove(i)
+        used_indices.add(i)
+    g.graph["root"] = root
+    return g
+
+def display_diagnostics(diagnostics):
+    mmm = diagnostics[0]["matrix"]
+    for d in diagnostics:
+        fig, ax = plt.subplots()
+        ax.imshow(mmm, vmin=0, vmax=1, cmap='gray_r')
+        title = None
+        if "selection" in d:
+            for i in d["rem_ind_arr"]:
+                for j in d["used_ind_arr"]:
+                    ax.add_patch(mpl.patches.Rectangle((j - 0.5, i - 0.5), 1, 1, fill=None, edgecolor='blue'))
+            i, j = d["edge"]
+            ax.add_patch(mpl.patches.Rectangle((j - 0.5, i - 0.5), 1, 1, fill=None, edgecolor='red'))
+            title = "Add Edge: {} --> {}".format(j, i)
+            ax.set_yticks(np.arange(mmm.shape[0]))
+            ax.set_yticklabels(np.arange(mmm.shape[0]))
+            ax.set_xticks(np.arange(mmm.shape[1]))
+            ax.set_xticklabels(np.arange(mmm.shape[1]))
+        elif "root" in d:
+            ax.add_patch(mpl.patches.Rectangle((-0.5, d["root"] - 0.5), d["matrix"].shape[1], 1, fill = None, edgecolor='red'))
+            title = "Select Root: {}".format(d["root"])
+        if title is not None:
+            ax.set_title(title)
+            print(title)
+
+def display_diagnostics_submatrices(diagnostics):
+    for d in diagnostics:
+        fig, ax = plt.subplots()
+        ax.imshow(d["matrix"], vmin=0, vmax=1, cmap='gray_r')
+        title = None
+        if "selection" in d:
+            i, j = d["selection"]
+            #print("col (x) = {}, row (y) = {}".format(i, j))
+            ax.add_patch(mpl.patches.Rectangle((j - 0.5, i - 0.5), 1, 1, fill=None, edgecolor='red'))
+            title = "Add Edge: {} --> {}".format(d["edge"][1], d["edge"][0])
+            ax.set_yticks(np.arange(len(d["rem_ind_arr"])))
+            ax.set_yticklabels(d["rem_ind_arr"])
+            ax.set_xticks(np.arange(len(d["used_ind_arr"])))
+            ax.set_xticklabels(d["used_ind_arr"])
+        elif "root" in d:
+            ax.add_patch(mpl.patches.Rectangle((-0.5, d["root"] - 0.5), d["matrix"].shape[1], 1, fill = None, edgecolor='red'))
+            title = "Select Root: {}".format(d["root"])
+        if title is not None:
+            ax.set_title(title)
+            print(title)
+
---
+++ simple-hier-topic-model/tree_extraction/GreedyWithStructure.py
@@ -0,0 +1,54 @@
+import numpy as np
+import networkx as nx
+
+def extract_tree_helper(X, root_index, branching_factors, indices):
+    g = nx.Graph()
+    root = indices[root_index]
+    g.add_node(root)
+    if len(branching_factors) == 0:
+        return g
+
+    num_children = branching_factors[0]
+    remaining_branching_factors = branching_factors[1:]
+    descendants_per_child = int(np.prod(remaining_branching_factors))
+
+    assert len(indices) >= num_children
+    assert X.shape[0] == X.shape[1]
+    assert X.shape[0] == len(indices), "{} vs {}".format(X.shape[0], len(indices))
+    if len(indices) == num_children:
+        for i in indices:
+            g.add_edge(root, i)
+        return g
+
+    mask_child_candidates = np.ones(indices.shape, dtype='bool')
+    mask_child_candidates[root_index] = False
+    children_indirect = np.argpartition(X[root_index, mask_child_candidates], -num_children)[-num_children:]
+    children = np.where(mask_child_candidates)[0][children_indirect]
+
+    mask_candidates = np.ones(indices.shape, dtype='bool')
+    mask_candidates[root_index] = False
+    mask_candidates[children] = False
+
+    for c in children:
+        to_recurse = [c]
+        indirect = np.argpartition(X[c, mask_candidates], -descendants_per_child)[-descendants_per_child:]
+        to_recurse.extend(np.where(mask_candidates)[0][indirect])
+        to_recurse_col = np.reshape(to_recurse, (-1, 1))
+        # Compute subtree and add to graph
+        Xsub = X[to_recurse_col, to_recurse_col.T]
+        remaining_indices = indices[to_recurse]
+        subtree = extract_tree_helper(Xsub, 0, remaining_branching_factors, remaining_indices)
+        g = nx.compose(g, subtree)
+        g.add_edge(root, indices[c])
+        # Update candidates
+        mask_candidates[to_recurse] = False
+
+    return g
+
+def extract(X, branching_factors):
+    indices = np.arange(X.shape[0])
+    p_node = X.sum(axis = 0)
+    root = np.argmax(p_node)
+    g = extract_tree_helper(X, root, branching_factors, indices)
+    g.graph["root"] = root
+    return g
