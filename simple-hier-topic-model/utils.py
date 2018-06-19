cd896296b7c3a320a6dc00ae97144c50c8418eaa
Andrew Leverentz <aleverentz@noreply>
2018-06-19 13:03:47 -0600

    Add some more utils

--- simple-hier-topic-model/utils.py
+++ simple-hier-topic-model/utils.py
@@ -38,3 +38,58 @@ def niceprint_str(X, precision = 4):
 
 def niceprint(*args, **kwargs):
     print(niceprint_str(*args, **kwargs))
+
+def nicesubplots(rows, cols, scale=4, **kwargs):
+    import matplotlib.pyplot as plt
+    dims = np.array([rows, cols])
+    return plt.subplots(*dims, figsize = dims[::-1] * scale, **kwargs)
+
+def _bfs_layout_helper(tree, source, spacing, center):
+    pos = dict()
+    results_by_child = []
+    for n in sorted(tree.neighbors(source)):
+        results_by_child.append(_bfs_layout_helper(tree, n, spacing, center))
+    pos[source] = center
+    width = 0.0
+    for i, (subtree_pos, subtree_width) in enumerate(results_by_child):
+        if i > 0:
+            width += spacing[0]
+        width += subtree_width
+    shiftx = -width / 2.0
+    for subtree_pos, subtree_width in results_by_child:
+        shiftx += subtree_width / 2.0
+        for k, (x, y) in subtree_pos.items():
+            pos[k] = (x + shiftx, y - spacing[1])
+        shiftx += subtree_width / 2.0 + spacing[0]
+    return pos, width
+
+def bfs_layout(G, sources=None, spacing=(1.0, 1.0), center=(0.0, 0.0)):
+    import networkx as nx
+    pos = dict()
+    if G.number_of_nodes() == 0:
+        return pos
+    if sources is None:
+        if "root" in G.graph:
+            sources = [G.graph["root"]]
+        else:
+            sources = []
+    if G.is_directed():
+        components = nx.algorithms.weakly_connected_component_subgraphs(G)
+    else:
+        components = nx.algorithms.connected_component_subgraphs(G)
+    cumulative_width = 0
+    for comp in components:
+        current_source = None
+        for s in sources:
+            if s in comp.nodes():
+                current_source = s
+                break
+        if current_source is None:
+            current_source = list(comp.nodes())[0]
+        tree = nx.bfs_tree(G, current_source)
+        current_pos, width = _bfs_layout_helper(tree, current_source, spacing, center)
+        for k in current_pos.keys():
+            current_pos[k] = (current_pos[k][0] + cumulative_width, current_pos[k][1])
+        pos.update(current_pos)
+        cumulative_width += width + spacing[0]
+    return pos
